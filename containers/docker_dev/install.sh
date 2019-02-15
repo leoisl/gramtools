d0c0b53e861e9288ae7c9079cdd16ba2c1eb52d9
Leandro Ishi Soares de Lima <leandro.ishi.lima@gmail.com>
2019-02-15 11:33:09 +0100

    Adding a docker_dev so that I can compile on my mac :(

---
+++ containers/docker_dev/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y \
+    build-essential \
+    libbz2-dev \
+    liblzma-dev \
+    cmake \
+    git \
+    python3-pip \
+    autoconf \
+    sudo \
+    wget \
+    zlib1g-dev
